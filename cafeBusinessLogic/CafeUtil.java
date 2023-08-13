498778a26e0e2d0b2b9471a6f225994163a0b98a
John <johnpua@outlook.com>
2023-08-13 16:59:26 -0700

    First Week

---
+++ cafeBusinessLogic/CafeUtil.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class CafeUtil {
+
+    // Method 1: getStreakGoal
+    public int getStreakGoal() {
+        return getStreakGoal(10); // By default, we'll use 10 weeks
+    }
