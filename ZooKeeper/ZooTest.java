fe44b1e19cf458dc46bccc27714c45025bbf2077
John <johnpua@outlook.com>
2023-08-14 00:08:58 -0700

    Second Week

--- ZooKeeper/ZooTest.java
+++ ZooKeeper/ZooTest.java
@@ -1,3 +1,24 @@
 public class ZooTest {
-    
+    public static void main(String[] args) {
+        // Gorilla Test
+        Gorilla g = new Gorilla();
+        g.throwSomething();
+        g.throwSomething();
+        g.throwSomething();
+        g.eatBananas();
+        g.eatBananas();
+        g.climb();
+        g.displayEnergy();
+
+        // Bat Test
+        Bat b = new Bat();
+        b.attackTown();
+        b.attackTown();
+        b.attackTown();
+        b.eatHumans();
+        b.eatHumans();
+        b.fly();
+        b.fly();
+        b.displayEnergy();
+    }
 }
