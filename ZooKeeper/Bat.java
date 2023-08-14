dff96c711983c0abe6661b7da856bc51e74607c1
John <johnpua@outlook.com>
2023-08-14 00:02:41 -0700

    Second Week

--- ZooKeeper/Bat.java
+++ ZooKeeper/Bat.java
@@ -1,3 +1,21 @@
-public class Bat {
-    
+public class Bat extends Mammal {
+
+    public Bat() {
+        this.energy = 300; // overriding the default energy for bats
+    }
+
+    public void fly() {
+        this.energy -= 50;
+        System.out.println("Whooosh! Bat is airborne!");
+    }
+
+    public void eatHumans() {
+        this.energy += 25;
+        System.out.println("Well, that happened...");
+    }
+
+    public void attackTown() {
+        this.energy -= 100;
+        System.out.println("Fire! The town is under attack by a bat!");
+    }
 }
