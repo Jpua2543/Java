be39903c5f8db864f5a699f64fa6bede4834dff1
John <johnpua@outlook.com>
2023-08-23 16:36:38 -0700

    Device

---
+++ Devices/Device.java
@@ -0,0 +1,7 @@
+class Device {
+    protected int battery = 100;
+
+    public void status() {
+        System.out.println("Battery remaining: " + battery);
+    }
+}
---
+++ Devices/DeviceTest.java
@@ -0,0 +1,14 @@
+public class DeviceTest {
+    public static void main(String[] args) {
+        Phone myPhone = new Phone();
+
+        myPhone.makeCall();
+        myPhone.makeCall();
+        myPhone.makeCall();
+
+        myPhone.playGame();
+        myPhone.playGame();
+
+        myPhone.charge();
+    }
+}
