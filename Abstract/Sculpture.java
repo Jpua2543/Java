28374a7924ee3f1529003bc2fec49671b39e1f19
John <johnpua@outlook.com>
2023-08-23 18:44:40 -0700

    Abstract

--- Abstract/Sculpture.java
+++ Abstract/Sculpture.java
@@ -1,3 +1,16 @@
-public class Sculpture {
-    
+public class Sculpture extends Art {
+    private String material;
+
+    public Sculpture(String title, String author, String description, String material) {
+        super(title, author, description);
+        this.material = material;
+    }
+
+    @Override
+    public void viewArt() {
+        System.out.println("Sculpture Title: " + title);
+        System.out.println("Artist: " + author);
+        System.out.println("Description: " + description);
+        System.out.println("Material: " + material);
+    }
 }
