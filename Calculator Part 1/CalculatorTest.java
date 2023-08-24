4ff1f750af5873407e7d0f97fee9a57d6ffa4c1d
John <johnpua@outlook.com>
2023-08-23 18:59:20 -0700

    Calculator Part 1

--- Calculator Part 1/CalculatorTest.java
+++ Calculator Part 1/CalculatorTest.java
@@ -1,3 +1,11 @@
 public class CalculatorTest {
-    
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        calculator.setOperandOne(10.5);
+        calculator.setOperation("+");
+        calculator.setOperandTwo(5.2);
+        calculator.performOperation();
+        System.out.println("Result: " + calculator.getResults()); // Output should be 15.7
+    }
 }
