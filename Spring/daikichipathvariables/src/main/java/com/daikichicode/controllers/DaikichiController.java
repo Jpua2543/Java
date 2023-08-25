2f6d7047fb43b1bafc5fddd65c8be3f990e204bf
John <johnpua@outlook.com>
2023-08-25 16:21:34 -0700

    Spring

--- Spring/daikichipathvariables/src/main/java/com/daikichicode/controllers/DaikichiController.java
+++ Spring/daikichipathvariables/src/main/java/com/daikichicode/controllers/DaikichiController.java
@@ -5,21 +5,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/daikichi")
 public class DaikichiController {
 
-    // Mapping for travel city
-    @RequestMapping("/daikichi/travel/{city}")
-    public String travelDestination(@PathVariable("city") String city) {
-        return "Congratulations! You will soon travel to " + city + "!";
+    @RequestMapping("")
+    public String welcome() {
+        return "Welcome";
     }
 
-    // Mapping for lotto number
-    @RequestMapping("/daikichi/lotto/{number}")
-    public String lottoPrediction(@PathVariable("number") int number) {
+    @RequestMapping("/today")
+    public String today() {
+        return "Today you will find luck in all your endeavors!";
+    }
+
+    @RequestMapping("/tomorrow")
+    public String tomorrow() {
+        return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+    }
+
+    @RequestMapping("/travel/{location}")
+    public String travel(@PathVariable("location") String location) {
+        return "Congratulations! You will soon travel to " + location + "!";
+    }
+
+    @RequestMapping("/lotto/{number}")
+    public String lotto(@PathVariable("number") int number) {
         if (number % 2 == 0) {
-            return "You will take a grand journey in the near future, but be weary of tempting offers.";
-        } else {
-            return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+            return "You will take a grand journey in the near future, but be weary of tempting offers";
         }
+        return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
     }
 }
