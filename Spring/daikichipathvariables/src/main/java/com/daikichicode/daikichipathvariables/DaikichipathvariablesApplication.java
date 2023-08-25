555de9b0d21e1ace21631c521adf2143ec0b99ee
John <johnpua@outlook.com>
2023-08-25 15:25:19 -0700

    SpringJava

---
+++ Spring/daikichipathvariables/src/main/java/com/daikichicode/daikichipathvariables/DaikichipathvariablesApplication.java
@@ -0,0 +1,13 @@
+package com.daikichicode.daikichipathvariables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikichipathvariablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichipathvariablesApplication.class, args);
+	}
+
+}
---
+++ Spring/daikichipathvariables/src/main/java/com/daikichicode/daikichipathvariables/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.daikichicode.daikichipathvariables;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DaikichipathvariablesApplication.class);
+	}
+
+}
