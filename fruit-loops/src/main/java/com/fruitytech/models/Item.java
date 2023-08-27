173a9c633f265d4fe567f931690e1b469a5a71fa
John <johnpua@outlook.com>
2023-08-27 01:36:12 -0700

    Fruit-loops

---
+++ fruit-loops/src/main/java/com/fruitytech/models/Item.java
@@ -0,0 +1,31 @@
+package com.fruitytech.models;
+
+public class Item {
+    // MEMBER VARIABLES
+    private String name;
+    private double price;
+
+    // CONSTRUCTOR
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    // GETTERS AND SETTERS
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
